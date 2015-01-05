32db49440aad5cff8c04e1725ce128ca75fb031f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2015-01-05 16:27:48 +0800

    Initial Commit

---
+++ ExpressRabbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ExpressRabbit
+//
+//  Created by kuaiditu on 15-1-5.
+//  Copyright (c) 2015年 kuaiditu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
